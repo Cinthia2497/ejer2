4037fec35a9fa96fc2083958f43a1df539bd096a
Cinthia2497 <euceda_cinthia@yahoo.com>
2021-06-25 21:09:11 -0600

    ejercicio2

--- ejer.py
+++ ejer.py
@@ -10,37 +10,37 @@ def edad(año):
     return 2021-año
 def menu():
     print("*********")
-    print("*        MENU         *")
+    print("*        MENU DE AREAS         *")
     print("1. Area del triángulo")
     print("*2. Area del cuadrado *")
     print("*3. Area del círculo  *")
     print("*4. Que edad tengo    *")
     print("*5.      Salir        *")
     print("*********")
-    opcion = int(input("Escoja la opcion: "))
+    opcion = int(input("Elejir la opcion: "))
     return opcion
 
 opcion = menu()
 while opcion != 5:
     if opcion == 1:
-        print("Calculo del area de un Triangulo")
+        print("Area de un Triangulo")
         base = int(input("ingrese la base: " ) )
         altura = int(input("ingrese la altura: "))
         calculo = areatriangulo(base,altura)
         print("Area del triangulo: " + str(calculo) + "\n")
     elif opcion == 2:
-        print("Calculo del area de un Cuadrado")
+        print("Area de un Cuadrado")
         lado = int(input("ingrese uno de los Lados: " ) )
         CalculoC = areacuadrado(lado)
         print("Area del Cuadrado es: " + str(CalculoC) + "\n")
     elif opcion == 3:
-        print("Calculo del area de un Circulo")
+        print("Area de un Circulo")
         r = int(input("ingrese el diametro: " ) )
         areaC =areacirculo(r)
         print("Area del Circulo: " + str(areaC) + "\n")
     elif opcion == 4:
-        print("Adivinar edad")
-        año = int(input("ingrese su año de Nacimiento " ) )
+        print("Que edad tengo")
+        año = int(input("Ingrese su año de Nacimiento " ) )
         edad2 =edad(año)
         print("Su edad es " + str(edad2) + "\n")
     else:
